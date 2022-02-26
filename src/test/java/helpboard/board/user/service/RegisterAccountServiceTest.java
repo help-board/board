80356b22b599c9d3ce6b6cd352ef35b929037226
Athlan <athlan@vgroup.pl>
2022-02-26 19:50:22 +0100

    account registration

--- src/test/java/helpboard/board/user/service/RegisterAccountServiceTest.java
+++ src/test/java/helpboard/board/user/service/RegisterAccountServiceTest.java
@@ -1,11 +1,13 @@
 package helpboard.board.user.service;
 
 import helpboard.board.user.domain.UserRepository;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatCode;
 
 @SpringBootTest
 class RegisterAccountServiceTest {
@@ -15,6 +17,11 @@ class RegisterAccountServiceTest {
 
     @Autowired
     UserRepository userRepository;
+
+    @AfterEach
+    void cleanup() {
+        userRepository.deleteAllInBatch();
+    }
 
     @Test
     void registerNewAccount() {
@@ -31,5 +38,16 @@ class RegisterAccountServiceTest {
                     assertThat(user.getUsername())
                             .isEqualTo(username);
                 });
+    }
+
+    @Test
+    void cannotRegisterIfUsernameAlreadyTaken() {
+        // given
+        var username = "username";
+        registerAccountService.register(username, "password");
+
+        assertThatCode(() -> registerAccountService.register(username, "password"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("Username already taken");
     }
 }

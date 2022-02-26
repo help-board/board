5ad006a2c92ddfe3e5802cec5551b936f40832f2
Athlan <athlan@vgroup.pl>
2022-02-26 16:28:29 +0100

    auth config

--- src/main/java/helpboard/board/user/SecurityConfiguration.java
+++ src/main/java/helpboard/board/user/SecurityConfiguration.java
@@ -2,21 +2,30 @@ package helpboard.board.user;
 
 import helpboard.board.user.domain.Role;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 
 @Configuration
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
     public void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable();
+
         http
                 .authorizeRequests()
                 .anyRequest()
                     .authenticated()
                     .and()
-                .httpBasic();
+                .formLogin()
+                    .loginPage("/auth/login")
+                    .permitAll()
+                    .and()
+                    .exceptionHandling() // 1
+                    .authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED));
     }
 
     @Override

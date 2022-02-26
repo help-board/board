2c4ee610aeab8de0c18be7375087f5d6a017c713
Athlan <athlan@vgroup.pl>
2022-02-26 13:28:15 +0100

    authentication

---
+++ src/main/java/helpboard/board/user/SecurityConfiguration.java
@@ -0,0 +1,29 @@
+package helpboard.board.user;
+
+import helpboard.board.user.domain.Role;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .anyRequest()
+                    .authenticated()
+                    .and()
+                .httpBasic();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("user")
+                .password("{noop}pass") // Spring Security 5 requires specifying the password storage format
+                .roles(Role.User.toString());
+    }
+}

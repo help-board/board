6b9512ddf4c7f23cc39cd8ae51495bd5db5c39b1
Athlan <athlan@vgroup.pl>
2022-02-27 15:26:59 +0100

    session cookie customization

--- src/main/java/helpboard/board/user/SessionConfig.java
+++ src/main/java/helpboard/board/user/SessionConfig.java
@@ -3,6 +3,8 @@ package helpboard.board.user;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
+import org.springframework.session.web.http.CookieSerializer;
+import org.springframework.session.web.http.DefaultCookieSerializer;
 
 @EnableRedisHttpSession
 public class SessionConfig {
@@ -10,5 +12,14 @@ public class SessionConfig {
     @Bean
     public LettuceConnectionFactory connectionFactory() {
         return new LettuceConnectionFactory();
+    }
+
+    @Bean
+    public CookieSerializer cookieSerializer() {
+        DefaultCookieSerializer serializer = new DefaultCookieSerializer();
+        serializer.setCookieName("auth_token");
+        serializer.setCookiePath("/");
+        serializer.setDomainNamePattern("^.+?\\.(\\w+\\.[a-z]+)$");
+        return serializer;
     }
 }
